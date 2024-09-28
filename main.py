db02772c8afd88ea6a1e5eca8eb6e26fbd08c508
pooriya <adibradp@gmail.com>
2024-09-29 03:29:10 +0330

    set web browser behavior

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Cookie": "session-id=136-3547298-1682643; session-id-time=2082787201l; csm-hit=tb:TDJZKRZFRPVC417N42Q5+sa-3M93R1XAKD51J95YY8AF-9SYXW0HHC4MZM2PA0W5P|1727567801448&t:1727567801448&adb:adblk_no; ad-oo=0; ci=e30; ubid-main=135-1309134-5172517; session-token=bNGUPrt7UrUV2vUeVgsCf6VMdkIIS/0IM/do13mElFwkIGiXPxo6TCV03b+/OITsG0cZdE5x34ftUZONiS85Zos89BtQlbCpKrwobYKCYemaqHhZfZVNSLUtdxtfuoLZmKlsejT4tW55i+IegaN/o7uFLDNHTHvoQpfwQ6c2M1L9EYse4jXYegD+DRmQV5jWhdZAON+nHr9NGgpqdDKWdj4jXHIVzOzVSmzFFqzxxjgw20WCgqIUWpO9YkX0Qc3VbrIexgbhxX6nZT0ECLMwESNsyJt5N+R6wikk6PTlvozb0duLWJdwTkQpIA5ac/47GeRzV4jY1y/louVOKohEbnkv+7BHuM9b",
+    "Host": "www.imdb.com",
+    "Priority": "u=0, i",
+    "Referer": "https://www.imdb.com/",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
+}
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
